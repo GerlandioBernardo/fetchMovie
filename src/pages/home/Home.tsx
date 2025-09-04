5470c241fe8142b79484aaf8bd649fafcda85986
Gerlandio Bernardo <gerlandios51@gmail.com>
2025-09-04 09:59:07 -0300

    feat: implementar pesquisa de filmes em tempo real

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import Header from '../../components/Header';
 import apiTmdb from '../../services/ApiTmdb';
 import type { typeMovie } from '../../types/TypeMovie';
 import { useModal } from '../../hook/useModal';
+import { FaExclamationCircle } from "react-icons/fa";
 
 export default function Home() {
 
@@ -12,13 +13,13 @@ export default function Home() {
   const [query, setQuery] = useState("");
   const [error, setError] = useState("");
 
-  const {openModal} = useModal();
+  const { openModal } = useModal();
 
-  async function fetchMovies(pageNumber: number){
+  async function fetchMovies(pageNumber: number) {
     try {
-      
+
       const response = await apiTmdb.get("/movie/popular", {
-        params:{
+        params: {
           page: pageNumber
         }
       });
@@ -32,42 +33,65 @@ export default function Home() {
     }
   }
 
-  async function searchMovie(title: string){
+  async function searchMovie(title: string) {
 
-    const result = movies.filter((m)=>{
-      m.title.toLowerCase().includes(title.toLowerCase());
+    const result = movies.filter((m) => {
+      return m.title.toLowerCase().includes(title.toLowerCase());
     })
 
-    if(result.length > 0){
-      setMovies(result);
+    if (result.length > 0) {
       setError("");
+      setMovies(result);
       return;
+    }
+    else {
+      try {
+        const response = await apiTmdb.get("/search/movie", {
+          params: {
+            query: title,
+            page: 1
+          }
+        });
+
+        if (response.data.results.length > 0) {
+          setError("");
+          setMovies(response.data.results);
+          setTotalPages(response.data.total_pages);
+        } else {
+          setMovies([]);
+          setError("No movies found");
+        }
+
+      } catch {
+        setError("Error searching for movie");
+      }
     }
   }
 
-  
-  useEffect(()=>{
-    if(query.trim() === ""){
+
+  useEffect(() => {
+    
+    if (query.trim() === "") {
       fetchMovies(page);
     }
-  },[page, query])
+  }, [page, query])
 
-  useEffect(()=>{
-    if(query.trim() === "") return;
+  useEffect(() => {
+    if (query.trim() === "") return;
 
-    const debounce = setTimeout(()=>{
+    const debounce = setTimeout(() => {
       searchMovie(query);
     }, 400);
 
-    return ()=> clearTimeout(debounce);
+    return () => clearTimeout(debounce);
 
   }, [query])
 
-  function handleSubmit(id: string){
-    
+  function handleSubmit(id: string) {
+
     const movie = movies.find(m => m.id === id);
-    if(movie){
-        openModal(movie);
+    if (movie) {
+      openModal(movie);
     }
 
   }
@@ -79,17 +103,20 @@ export default function Home() {
         <h1 className='text-center text-red-600 pb-2 font-bold text-[18px]'>Search Movie</h1>
         <div className='flex justify-center items-center '>
           <div>
-            <input onChange={(event)=> setQuery(event.target.value)}  value={query}
-            className='border-2 border-red-800 focus:border-red-500 pl-2
+            <input onChange={(event) => setQuery(event.target.value)} value={query}
+              className='border-2 border-red-800 focus:border-red-500 pl-2
               outline-none w-100 h-9 text-[16px] text-gray-200 placeholder:text-gray-400'
               type="text" placeholder='Enter the name of the movie' required />
           </div>
         </div>
       </section>
 
-      <div className='mt-7'>
-        {error && <p className="text-red-600 mt-2 text-center">{error}</p>}
-      </div>
+      {error && (
+        <div className='flex flex-col items-center mt-6 text-gray-400'>
+          <FaExclamationCircle size={40} />
+          <p className='text-center'>{error}</p>
+        </div>
+      )}
 
       <section className='grid grid-cols-5 mt-15 justify-items-center pl-11 pr-11'>
         {movies.map((movie) => (
@@ -104,9 +131,9 @@ export default function Home() {
               />
             </div>
             <div >
-               <p className="text-yellow-400 font-bold mb-2 flex justify-center items-center">⭐ Nota: {movie.vote_average}</p>
+              <p className="text-yellow-400 font-bold mb-2 flex justify-center items-center">⭐ Nota: {movie.vote_average}</p>
             </div>
-            <button onClick={()=> handleSubmit(movie.id)} className='bg-gradient-to-r from-blue-400 via-blue-700 to-blue-900
+            <button onClick={() => handleSubmit(movie.id)} className='bg-gradient-to-r from-blue-400 via-blue-700 to-blue-900
             transition-colors ease-in-out duration-500 transform 
              text-white text-[16px] font-bold pt-1.5 pb-1.5 rounded w-50 cursor-pointer 
               hover:-translate-y-[1px] hover:scale-[1.01]'>
@@ -119,19 +146,19 @@ export default function Home() {
       </section>
       <section>
         {query.trim() === "" && (
-        <div className="flex justify-center gap-2 mt-6 flex-wrap">
-          {Array.from({ length: Math.min(totalPages, 10) }, (_, i) => i + 1).map(num => (
-            <button
-              key={num}
-              onClick={() => setPage(num)}
-              className={`px-3 py-1 rounded cursor-pointer ${page === num ? "bg-red-600 text-white" : "bg-gray-700 text-gray-300"
-                }`}
-            >
-              {num}
-            </button>
-          ))}
-          
-        </div>
+          <div className="flex justify-center gap-2 mt-6 flex-wrap">
+            {Array.from({ length: Math.min(totalPages, 10) }, (_, i) => i + 1).map(num => (
+              <button
+                key={num}
+                onClick={() => setPage(num)}
+                className={`px-3 py-1 rounded cursor-pointer ${page === num ? "bg-red-600 text-white" : "bg-gray-700 text-gray-300"
+                  }`}
+              >
+                {num}
+              </button>
+            ))}
+
+          </div>
         )}
       </section>
 
